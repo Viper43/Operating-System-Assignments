1eeab531b1dcc335c263c0dc21fe6a4caf10c982
Viper43 <rickk2014628@gmail.com>
2020-11-03 21:00:50 +0530

    v1.0

---
+++ Assignment !/Find_Initiators.py
@@ -0,0 +1,55 @@
+no_OfNodes = int(input('Enter the no of nodes:  '))
+front = -1
+queue = []
+
+
+def enqueue(x):
+    global front, queue
+    if front == -1:
+        front = 0
+    queue.append(x)
+
+
+def dequeue():
+    global front, no_OfNodes
+    front += 1
+    if front <= no_OfNodes - 1 and front < len(queue):
+        bfs(queue[front])
+
+
+def bfs(x):
+    global graph, no_OfNodes
+    if x not in queue:
+        enqueue(x)
+
+    for i in range(no_OfNodes):
+        if graph[x][i] and i not in queue:
+            enqueue(i)
+
+    dequeue()
+
+
+graph = [[0 for i in range(no_OfNodes)] for j in range(no_OfNodes)]
+vertices = input("enter the name of vertices:    ")
+vertices = vertices.replace(" ", "")
+while True:
+    print("enter the directed connected vertices")
+    v1 = (input())[0]
+    v2 = (input())[0]
+    graph[vertices.index(v1)][vertices.index(v2)] = 1
+
+    resp = (input("More connected vertices?(Y or y / N or n):  "))[0]
+    if resp == 'n' or resp == 'N':
+        break
+print(graph)
+setOfInit = []
+
+for i in range(no_OfNodes):
+    bfs(i)
+    print(len(queue))
+    if len(queue) == no_OfNodes:
+        setOfInit.append(vertices[i])
+    front = -1
+    queue.clear()
+
+print(setOfInit)
